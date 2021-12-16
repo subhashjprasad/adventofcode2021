5b4a5361adc378a5a1f65d7e43785f2206033a1d
Subhash Prasad <93240058+subhashjprasad@users.noreply.github.com>
2021-12-15 17:21:18 -0800

    Update adventofcodeday12.cpp

--- adventofcodeday12.cpp
+++ adventofcodeday12.cpp
@@ -12,22 +12,103 @@ void search(string center, string visited) {
 
     for (int i = 0; i < v.size(); i++) {
 
-        if (v[i].substr(0, center.length()) == center) {
+        string before = v[i].substr(0, v[i].find("-"));
+        string after = v[i].substr(v[i].find("-") + 1, v[i].length());
 
-            if (islower((v[i].substr(v[i].find("-") + 1, v[i].length() - v[i].find("-")))[0])) {
+        if (before == center) {
 
-                if ((v[i].substr(v[i].find("-") + 1, v[i].length() - v[i].find("-"))) == "end") {
+            if (islower(after[0])) {
+
+                if (after == "end") {
 
                     paths++;
 
-                } else if (visited.find((v[i].substr(v[i].find("-") + 1, v[i].length() - v[i].find("-")))) == string::npos) {
+                } else if (visited.find(after) == string::npos) {
 
-                    search((v[i].substr(v[i].find("-") + 1, v[i].length() - v[i].find("-"))), visited + "," + v[i].substr(v[i].find("-") + 1, v[i].length() - v[i].find("-")));
+                    search(after, visited + "," + after);
 
                 }
             } else {
 
-                search((v[i].substr(v[i].find("-") + 1, v[i].length() - v[i].find("-"))), visited);
+                search(after, visited);
+            }
+        }
+
+        if (after == center) {
+
+            if (islower(before[0])) {
+
+                if (before == "end") {
+
+                    paths++;
+
+                } else if (visited.find(before) == string::npos) {
+
+                    search(before, visited + "," + before);
+
+                }
+            } else {
+
+                search(before, visited);
+            }
+        }
+    }
+}
+
+void search2(string center, string visited, string twice) {
+
+    for (int i = 0; i < v.size(); i++) {
+
+        string before = v[i].substr(0, v[i].find("-"));
+        string after = v[i].substr(v[i].find("-") + 1, v[i].length());
+
+        if (before == center) {
+
+            if (islower(after[0])) {
+
+                if (after == "end") {
+
+                    if ((twice == "") || (visited.find(twice) != string::npos)) {
+                        paths++;
+                    }
+
+                } else if (visited.find(after) == string::npos) {
+
+                    search2(after, visited + "," + after, twice);
+
+                    if (twice == "") {
+                        search2(after, visited, twice + after);
+                    }
+
+                }
+            } else {
+
+                search2(after, visited + "," + after, twice);
+            }
+        }
+
+        if (after == center) {
+
+            if (islower(before[0])) {
+
+                if (before == "end") {
+
+                    if ((twice == "") || (visited.find(twice) != string::npos)) {
+                        paths++;
+                    }
+
+                } else if (visited.find(before) == string::npos) {
+
+                    search2(before, visited + "," + before, twice);
+
+                    if (twice == "") {
+                        search2(before, visited, twice + before);
+                    }
+
+                }
+            } else {
+
+                search2(before, visited + "," + before, twice);
             }
         }
     }
@@ -51,9 +132,28 @@ void part1() {
     cout << paths << endl;
 }
 
+void part2() {
+
+    ifstream inFile;
+
+    inFile.open("C:\\Users\\Subhash\\c++\\textfiles\\adventofcodeday12input.txt");
+
+    string x;
+
+    while (inFile >> x) {
+        v.push_back(x);
+    }
+
+    search2("start", "start", "");
+
+    cout << paths << endl;
+}
+
 int main() {
 
     part1();
+
+    part2();
 
     return 0;
 }
